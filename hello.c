1968010a344b5bafe16612659baab017a510d83d
Cpowell14 <54563043+Cpowell14@users.noreply.github.com>
2019-08-29 09:43:23 -0500

    Added Major
    Added a print line for my major

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Carter Powell
+ * Date: 2019/29/08
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Carter Powell\n");
+
+  printf("Computer Science\n");
+
+  return 0;
+}
